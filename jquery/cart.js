ae7362a0847d80050cb26d767639c15354a29f5c
TanTan969 <yephonepaing777@gmail.com>
2022-07-13 15:00:36 +0630

    tab

--- jquery/cart.js
+++ jquery/cart.js
@@ -1,40 +1,65 @@
 $(function () {
     getData();
-
-    $("button.atcbtn").on("click",function () {
-        var id = $(this).data("id");
-        var name = $(this).data("name");
-        var price = $(this).data("price");
-
-        var item = {
-            id: id,
-            name: name,
-            price: price,
-            qty: 1
+  
+    $("button.addtoCart_btn").on("click", function () {
+      let id = $(this).data("id");
+      let name = $(this).data("name");
+      let price = $(this).data("price");
+  
+      let item = {
+        id: id,
+        name: name,
+        price: price,
+        qty: 1,
+      };
+      // console.log(item);
+  
+      let cartStr = localStorage.getItem("cart");
+      if (!cartStr) {
+        var itemArr = new Array();
+        itemArr.push(item);
+      } else {
+        itemArr = JSON.parse(cartStr);
+        for (let i = 0; i < itemArr.length; i++) {
+          if (itemArr[i].name === name) {
+            itemArr[i].qty += 1;
+            var done = true;
+            break;
+          }
         }
-        // console.log(item)
-
-        var cartStr = localStorage.getItem('cart');
-        if(!cartStr){
-            var itemArr = new Array(); // first time
-        }else{
-            var itemArr = JSON.parse(cartStr) 
-        }
-        itemArr.push(item); // 
-        
-        localStorage.setItem('cart', JSON.stringify(itemArr));
-    })
-
-    function getData(){
-        var cartStr = localStorage.getItem('cart');
-        var data="";
-
-        if(!cartStr){
-            data += `Your Cart is Empty!`;
-        }else{
-            data += `Show items here!`
-        }
-
-        $("#cartitems").html(data);
+        if (!done) itemArr.push(item);
+      }
+  
+      localStorage.setItem("cart", JSON.stringify(itemArr));
+      getData();
+    });
+  
+    function getData() {
+      let cartStr = localStorage.getItem("cart");
+      var cartArr = JSON.parse(cartStr);
+  
+      var data = "";
+      if (!cartStr) {
+        data = `<p class="empty">Your Cart is Empty!</p>`;
+        $("#cart").html(data);
+      } else {
+        var items = cartArr.map(function (item) {
+          return `<tr>
+            <td>${item.name}</td>
+            <td>${item.price}</td>
+            <td>${item.qty}</td>
+            <td>${item.price * item.qty}</td>
+          </tr>`;
+        });
+        data += items;
+        $("tbody.product_data").html(data);
+        var prices = cartArr.map(function (item) {
+          return item.price * item.qty;
+        });
+        data = prices.reduce((sum, current) => {
+          return (sum += current);
+        }, 0);
+      }
+      $(".total_cost").html(data);
     }
-})
+  });
